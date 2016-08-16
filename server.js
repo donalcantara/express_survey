2734c935783711710c6c299040784764e45bb01d
Don Alcantara <don.alcantara@yahoo.com>
2016-08-16 16:35:26 -0700

    minor tweaks

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var express = require('express');
 var path = require('path');
 //create the express app
 var app = express();
+//require mongoose
+var mongoose = require('mongoose');
 //require body-parser
 var bodyParser = require('body-parser');
+//connect mongoose to mongodb
+mongoose.connect('mongodb://localhost/basic_mongoose');
 //use body parser
 app.use(bodyParser.urlencoded({extended: true}));
 //to view static files
@@ -18,15 +22,26 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
 	res.render('index');
 })
-app.post('/results', function(req,res){
-	var user_data = {
-		name: req.body.name,
-		email: req.body.email,
-		location: req.body.location,
-		language: req.body.language,
-		comment: req.body.comment
-	}
-	res.render('results', {userdata: user_data});
+
+var UserSchema = new mongoose.Schema({
+	name: String,
+	age: Number
+})
+mongoose.model('User', UserSchema); 
+var User = mongoose.model('User')
+
+app.post('/users', function(req,res){
+	console.log('POST DATA', req.body);
+	var user = new User ({name: req.body.name, age: req.body.age});
+	// .save saves the info to the database
+	user.save(function(err){
+		if(err){
+			console.log("Something done gone wrong");
+		} else {
+			console.log('Successfully added user!');
+			res.redirect('/');
+		}
+	})
 })
 //which port is the server listening to?
 app.listen(8000, function(){
